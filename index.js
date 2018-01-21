d13f0bc0aa3cf1d285524cf24ca233ae74f93fcf
HUNBIGBOSS <szevike_@vipmail.hu>
2018-01-21 18:37:42 +0100

    No comment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-
-app.get('/', function (req, res) { 
-	res.send('hello world');
+app.get('/', function (req, res) {
+	res.sendFile('/root/szamlakezelo/index.html');
 });
+
+
 
 app.listen(3000);
