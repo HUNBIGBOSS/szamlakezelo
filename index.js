2d2bc08325126923b69875716eae9d7ea1b86021
HUNBIGBOSS <szevike_@vipmail.hu>
2018-01-27 16:42:38 +0100

    A saját kódok és mások kódjai

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
-app.get('/', function (req, res) {
-	res.sendFile('/root/szamlakezelo/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	var obj = { "Adat":"van", "Igaz":"Talán" };
+	obj.title = 'title';
+	obj.data = 'data';
+	
+	console.log('params: ' + JSON.stringify(req.params));
+	console.log('body: ' + JSON.stringify(req.body));
+	console.log('query: ' + JSON.stringify(req.query));
+	console.log(obj);
+	
+	res.header('Content-type','application/json');
+	res.header('Charset','utf8');
+	res.send(req.query.callback + '('+ JSON.stringify(obj) + ');');
 });
 
+app.post('/', function(req, res){
+	var obj = {};
+	console.log('body: ' + JSON.stringify(req.body));
+	res.send(req.body);
+});
 
 
 app.listen(3000);
